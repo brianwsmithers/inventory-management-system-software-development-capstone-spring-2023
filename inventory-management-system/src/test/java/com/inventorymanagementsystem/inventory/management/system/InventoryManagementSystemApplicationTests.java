7ec5930646c364974e5d1400ff346e75aa90116a
Brian Smithers <56320061+brianwsmithers@users.noreply.github.com>
2023-02-06 09:30:01 -0500

    Initialize attempt 2...
    Hopefully this works!!!

---
+++ inventory-management-system/src/test/java/com/inventorymanagementsystem/inventory/management/system/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventorymanagementsystem.inventory.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
