0fb35f8094aca3022b64aa69d01621b50f29fd11
Elleni <ellu.miru@gmail.com>
2023-02-14 17:55:05 -0500

    fixed_customer_controller

--- inventory-management-system/src/main/java/com/inventorymanagementsystem/inventory/management/system/controllers/CustomerController.java
+++ inventory-management-system/src/main/java/com/inventorymanagementsystem/inventory/management/system/controllers/CustomerController.java
@@ -1,12 +1,15 @@
 package com.inventorymanagementsystem.inventory.management.system.controllers;
 
 import com.inventorymanagementsystem.inventory.management.system.domain.Customer;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CustomerController {
 
-    @GetMapping(value = "v1/Customer/{customer")
+    @GetMapping(value = "v1/customers/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer getCustomerById(@PathVariable("customerId") Integer id) {
      return new Customer();
     }
