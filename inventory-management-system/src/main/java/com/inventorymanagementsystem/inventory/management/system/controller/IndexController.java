aac0b30d1aa9191614a25e7caf94d9810269e9c1
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2023-04-08 20:49:27 -0400

    Chase UI sprint 3 4 (#10)
    * adds commands to run files in templates folder using thymeleaf
    
    * creates new files that handle interactions
    
    * updates the html files with added forms and actions to interact with the spring application when run
    
    * Adds forms and functionality for form inputs to be stored
    
    * Adds new nav links to new html pages
    
    * adds thymeleaf tables to show updates to tables when an item is added, deleted, or updated

--- inventory-management-system/src/main/java/com/inventorymanagementsystem/inventory/management/system/controller/IndexController.java
+++ inventory-management-system/src/main/java/com/inventorymanagementsystem/inventory/management/system/controller/IndexController.java
@@ -1,35 +1,107 @@
 package com.inventorymanagementsystem.inventory.management.system.controller;
 
-import com.inventorymanagementsystem.inventory.management.system.model.Product;
+
+import com.inventorymanagementsystem.inventory.management.system.data.CustomerDAO;
+import com.inventorymanagementsystem.inventory.management.system.domain.Customer;
+import com.inventorymanagementsystem.inventory.management.system.domain.Product;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import java.util.*;
+
 
 @Controller
 public class IndexController {
 
     HashMap<Integer, Product> products = new HashMap<>();
-    int id = 1;
+
+    HashMap<Integer, Customer> customers = new HashMap<>();
+
+    CustomerDAO customerDAO = new CustomerDAO();
+
+    int prodId = 1;
+    int custId = 1;
 
     public IndexController(){
-        products.put(id, new Product("om14", "yes", "1", "1", "1"));
+        products.put(prodId, new Product("om14", "yes", "1", "1", "1"));
+        prodId++;
+
+        products.put(prodId, new Product("eb41", "wireless earbuds", "100", "100", "100"));
+        prodId++;
+
+        products.put(prodId, new Product("ud20", "usb drive", "1000", "400", "1"));
+
+        customers.put(custId, new Customer(custId, "Chase", "Harrod", "123 Sesame Street", "1234567890",
+                "test1@gmail.com", 1));
+        custId++;
+        customers.put(custId, new Customer(custId, "Brian", "Smithers", "1600 Pennsylvania Ave", "2345678901",
+                "test2@gmail.com", 2));
+        custId++;
+        customers.put(custId, new Customer(custId, "Eline", "Gidey", "100 Hollywood Blvd", "3456789012",
+                "test3@gmail.com", 3));
     }
 
-    @GetMapping("/")
+    /**
+     * Get request that loads the products page and dynamically updates the rows of the table element on the html page
+     */
+    @RequestMapping("/products")
+    public String products(Model m){
+        List<Product> rows = new ArrayList<>();
+
+        for (Integer i: products.keySet()){
+            rows.add(products.get(i));
+
+        }
+        m.addAttribute("productRows", rows);
+
+        return "products";
+    }
+
+    /**
+     * * Get request that loads the customers page and dynamically updates the rows of the table element on the html page
+     * @param m
+     * @return customers.html page with customers table
+     */
+    @RequestMapping("/customers")
+    public String customers(Model m){
+        List<Customer> rows = new ArrayList<>();
+
+        for (Integer i: customers.keySet()){
+            rows.add(customers.get(i));
+
+        }
+        m.addAttribute("customerRows", rows);
+
+        return "customers";
+    }
+
+    /**
+     * loads the index.html page when application is run
+     * @return index.html page
+     */
+    @RequestMapping("/")
+
     public String index(){
         return "index";
     }
 
 
+
+    /**
+     * Processes a post request and binds the passed Product object to the model
+     * adds the Product object as a new entry to the products HashMap/Database
+     * @param prod - Product object
+     * @return index.html page
+     */
     @PostMapping("/addProduct")
     public String addProduct(@ModelAttribute Product prod){
 
-        id++;
-        products.put(id, prod);
+        prodId++;
+        products.put(prodId, prod);
+
 
         System.out.println(products.toString());
 
@@ -37,13 +109,19 @@ public class IndexController {
     }
 
 
-    @GetMapping("/removeProduct")
-    public String removeProduct(@ModelAttribute String id){
+    /**
+     * removes the product with the specified product id attribute from the HashMap/Database
+     * @param id
+     * @return index.html page
+     */
+    @GetMapping ("/removeProduct")
+    public String removeProduct(@ModelAttribute("id") String id){
 
         System.out.println(id);
         for(int i = 1; i <= products.keySet().size(); i++){
-            if(products.get(i).getId() == (id)){
-                products.remove(i, products.get(id));
+            if(products.get(i).getId().equalsIgnoreCase(id)){
+                products.remove(i);
+
             }
         }
 
@@ -53,10 +131,16 @@ public class IndexController {
     }
 
 
-    //update function - replaces entire object in arraylist currently
+
+    /**
+     * replaces an existing product in the HashMap/Database
+     * @param newProd
+     * @return index.html page
+     */
     @GetMapping("/updateProduct")
     public String updateProduct(@ModelAttribute Product newProd){
-        for (int i = 1; i <= products.keySet().size(); i++){
+        for (int i = prodId; i <= products.keySet().size(); i++){
+
                 if (products.get(i).getId().equalsIgnoreCase(newProd.getId())){
                     System.out.println("Old Product: " + products.get(i).toString());
 
@@ -66,37 +150,98 @@ public class IndexController {
                 }
             }
 
+        return "index";
+    }
+
+
+
+    @PostMapping("/addCustomer")
+    public String addCustomer(@ModelAttribute Customer cust){
+
+        custId++;
+        cust.setCustomerId(custId);
+        customers.put(custId, cust);
+
+//        customerDAO.save(cust);
+
+
+        System.out.println(customers.toString());
+
 
         return "index";
     }
 
-    @GetMapping("/result")
-    public String getProduct(Model model, @ModelAttribute("id") String id){
-        Product prod;
-        for(int i = 1; i <= products.keySet().size(); i++){
-            if(id.equalsIgnoreCase(products.get(i).getId())){
-                prod = products.get(i);
-                model.addAttribute("id", prod.getId());
-                model.addAttribute("desc", prod.getDesc());
-                model.addAttribute("currStock", prod.getCurrStock());
-                model.addAttribute("soQty", prod.getSoQty());
-                model.addAttribute("poQty", prod.getPoQty());
-                break;
+
+
+    /**
+     * removes the product with the specified product id attribute from the HashMap/Database
+     * @param id
+     * @return index.html page
+     */
+    @GetMapping ("/removeCustomer")
+    public String removeCustomer(@ModelAttribute("customerId") long id){
+
+        System.out.println(id);
+        for(int i = 1; i <= customers.keySet().size(); i++){
+            if(customers.get(i).getCustomerId() == id){
+                customers.remove(i);
             }
         }
 
+        System.out.println(customers.toString());
 
-        return "result";
+        return "index";
     }
+
+
+    /**
+     * replaces an existing customer in the HashMap/Database
+     * @param newCust
+     * @return index.html page
+     */
+    @GetMapping("/updateCustomer")
+    public String updateCustomer(@ModelAttribute Customer newCust, Model m){
+        for (Integer i: customers.keySet()){
+            if (customers.get(i).getCustomerId() == newCust.getCustomerId()){
+                System.out.println("Old Customer: " + customers.get(i).toString());
+
+                customers.replace(i, newCust);
+
+                System.out.println("New Customer: " + customers.get(i).toString());
+            }
+        }
+        return "index";
+    }
+
+
+    /**
+     * loads the home.html page when the nav link is clicked
+     * @return home.html page
+     */
 
     @RequestMapping("/home")
     public String home(){
         return "home";
     }
 
+    /**
+     * loads the login.html page when the nav link is clicked
+     * @return login.html page
+     */
+
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
+
+    /**
+     * loads the index.html page when the nav link is clicked
+     * @return index.html page
+     */
+    @RequestMapping("/index")
+    public String indexFromOthers(){
+        return "index";
+    }
+
 
 }
